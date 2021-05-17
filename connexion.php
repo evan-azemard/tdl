8132df27f5e19d7e1beb8699c4ad2ad3dfecba91
Nahems <clement.nahmens@laplateforme.io>
2021-05-17 12:39:51 +0200

    correction query

--- connexion.php
+++ connexion.php
@@ -2,9 +2,9 @@
 if (!isset($_SESSION["id"])) {
     if (isset($_POST["login"]) && isset($_POST["password"])) {
         $stmt = new PDO("mysql:host=localhost;dbname=tdl", "root", "");
-        $usr = $stmt->query("SELECT nom, password, id FROM connexion WHERE login = '" . $_POST["login"] . "'")->fetch(PDO::FETCH_ASSOC);
-        if (password_verify($_POST["password"], $usr["password"])) {
-            echo json_encode($usr);
+        $user = $stmt->query("SELECT nom, password, id FROM connexion WHERE login = '" . $_POST["login"] . "'")->fetch(PDO::FETCH_ASSOC);
+        if (password_verify($_POST["password"], $user["password"])) {
+            echo json_encode($user);
         } else {
             echo "err";
         }
